151f83cbd1df11c874771d2add11e8d3077c453f
DanFC <danielclausen12@gmail.com>
2024-04-09 00:05:43 -0700

    componente settings

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,23 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import App from "../App";
 import LoginPage from "../pages/login/LoginPage";
+import Settings from "../components/Configuration";
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/configuration",
+    element: <Settings />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+    errorElement: <ErrorPage />,
+  },
+]);
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/login",
-      element: <LoginPage />, 
-    },
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-
-      ],
-      errorElement: <ErrorPage/>,
-    },
-  ],
-);
-
-export default router; 
+export default router;

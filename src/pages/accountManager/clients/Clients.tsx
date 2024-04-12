0106ec5819899d3955ea87778f41acf4a52d2864
Andrés Martínez <autummata@gmail.com>
2024-04-12 12:20:11 -0700

    Add: Basic Structure to Client Cards

---
+++ src/pages/accountManager/clients/Clients.tsx
@@ -0,0 +1,10 @@
+import ClientCard from "../../../components/accountManager/clients/ClientCard"
+
+const Clients = () => {
+    return (
+        <>
+        <ClientCard clientName="Google" imgURL="https://i.imgur.com/6pzmgPW.png" clientDescription=":D"/></>
+    )
+}
+
+export default Clients;

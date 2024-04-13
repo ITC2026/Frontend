f34ff5badc995c0fae7621e2b821675a350c253d
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-04-12 20:46:13 -0700

    Merge Conflicts

--- src/pages/login/LoginCard.tsx
+++ src/pages/login/LoginCard.tsx
@@ -42,7 +42,7 @@ const LoginCard = () => {
         
         <button 
           type="submit" 
-          className="btn btn-primary encora-purple-button" 
+          className="btn btn-primary encora-purple-button " 
           id="login-button"
         >
           Submit

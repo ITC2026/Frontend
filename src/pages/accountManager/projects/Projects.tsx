744dcfc419524f1235aa35a2f79e18cb374d9ae5
Andrés Martínez <autummata@gmail.com>
2024-04-16 00:52:37 -0700

    Add: Project Tabs, Register Project button upon entering the 'Projects in Preparation' tab

--- src/pages/accountManager/projects/Projects.tsx
+++ src/pages/accountManager/projects/Projects.tsx
@@ -5,17 +5,46 @@ import { getAllProjects } from "../../../api/ProjectAPI";
 import { useState, useEffect } from "react";
 
 const project_structure = {
-  "project_title": "Nombre del Proyecto",
-  "client_name": "Cliente",
-  "account_manager": "Account Manager",
-  "percentage_complete": "% de Cobertura",
-  "start_date": "Fecha de Apertura",
-  "expiration_date": "Fecha de Cierre",
+  project_title: "Nombre del Proyecto",
+  client_name: "Cliente",
+  account_manager: "Account Manager",
+  percentage_complete: "% de Cobertura",
+  start_date: "Fecha de Apertura",
+  expiration_date: "Fecha de Cierre",
+};
+
+interface PropTab {
+  selected: string;
+  setSelected: (selected: string) => void;
 }
 
+const TabNav = (props: PropTab) => {
+  const projectTypeList = [
+    "Proyectos en Preparación",
+    "Proyectos Activos",
+    "Proyectos Cerrados",
+  ];
+
+  return (
+    <ul className="nav nav-pills nav-fill">
+      {projectTypeList.map((type: string, index: number) => (
+        <li key={index} className="nav-item">
+          <a
+            key={index}
+            className={`nav-link ${props.selected === type ? "encora-purple active text-light" : "text-body"}`}
+            onClick={() => props.setSelected(type)}
+          >
+            {type}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const ProjectPage = () => {
   const [projects, setProjects] = useState<Project[]>([]);
+  const [selected, setSelected] = useState<string>("Proyectos en Preparación");
 
   useEffect(() => {
     getAllProjects().then((data: unknown) => {
@@ -26,9 +55,17 @@ const ProjectPage = () => {
 
   return (
     <div>
-      <h1>Proyectos</h1>
+      <TabNav selected={selected} setSelected={setSelected} />
+      <div className="project-header">
+        <h1>Proyectos</h1>
+        {selected === "Proyectos en Preparación" ? (
+          <button className="project-register encora-purple-button text-light"> Registrar Proyecto </button>
+        ) : (
+          ""
+        )}
+      </div>
 
-      <TableView entity = {projects} types = {project_structure}/>
+      <TableView entity={projects} types={project_structure} />
     </div>
   );
 };

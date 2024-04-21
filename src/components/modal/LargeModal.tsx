e3b2b9244aedaba3cb809635ec9d763756b14ede
DLujan04 <A01254903@tec.mx>
2024-04-20 23:47:08 -0700

    Small changes to Postulate Bench Info modal

--- src/components/modal/LargeModal.tsx
+++ src/components/modal/LargeModal.tsx
@@ -1,9 +1,19 @@
+import React, { ReactNode } from 'react';
 import "../../index.css";
 import "./style/LargeModal.css";
-import { LargeModalType, EntityFormType, LargeModalProps } from "./modalType.ts";
+import { LargeModalType, EntityFormType } from "./modalType.ts";
 import { useDisableInput } from "../../hooks/useDisableInput.tsx"
 import CheckboxFormGroup from "./CheckboxFormGroup.tsx";
 import GenericFormGroup from "./GenericFormGroup.tsx";
+
+interface LargeModalProps {
+  titleModal: string;
+  btnArray: JSX.Element[]; 
+  typeOfModal: LargeModalType;
+  entityForm: EntityFormType;
+  onClose: () => void;
+  children?: ReactNode;
+}
 
 const renderModalContent = (typeOfModal: LargeModalType, entityForm: EntityFormType, disableInput?: Number[], setDisableInput?: React.Dispatch<React.SetStateAction<Number[]>>) => {
   const { formStructure } = entityForm;
@@ -12,29 +22,29 @@ const renderModalContent = (typeOfModal: LargeModalType, entityForm: EntityFormT
     case "info":
       return Object.keys(formStructure).map((nameLabel: string) => {
         if (formStructure[nameLabel].inputType === "checkbox") {
-          return <CheckboxFormGroup 
-            nameLabel={nameLabel} 
+          return <CheckboxFormGroup
+            nameLabel={nameLabel}
             disableInput={true}
           />
         }
-        return <GenericFormGroup 
-          nameLabel={nameLabel} 
+        return <GenericFormGroup
+          nameLabel={nameLabel}
           disableInput={true}
-          inputType={formStructure[nameLabel].inputType} 
+          inputType={formStructure[nameLabel].inputType}
         />
       });
     case "register":
       return Object.keys(formStructure).map((nameLabel: string, index: number) => {
         if (formStructure[nameLabel].inputType === "checkbox") {
-          return <CheckboxFormGroup 
-            nameLabel={nameLabel} 
+          return <CheckboxFormGroup
+            nameLabel={nameLabel}
             disableInput={disableInput ? disableInput.includes(index) : false}
             indexFromDisabledInput={formStructure[nameLabel].whichInputCanDisabled}
             setDisableInput={setDisableInput}
           />
         } else if (formStructure[nameLabel].canBeModified) {
-          return <GenericFormGroup 
-            nameLabel={nameLabel} 
+          return <GenericFormGroup
+            nameLabel={nameLabel}
             disableInput={disableInput ? disableInput.includes(index) : false}
             inputType={formStructure[nameLabel].inputType}
           />
@@ -43,15 +53,15 @@ const renderModalContent = (typeOfModal: LargeModalType, entityForm: EntityFormT
     case "modify":
       return Object.keys(formStructure).map((nameLabel: string, index: number) => {
         if (formStructure[nameLabel].inputType === "checkbox") {
-          return <CheckboxFormGroup 
-            nameLabel={nameLabel} 
+          return <CheckboxFormGroup
+            nameLabel={nameLabel}
             disableInput={disableInput ? disableInput.includes(index) : false}
             indexFromDisabledInput={formStructure[nameLabel].whichInputCanDisabled}
             setDisableInput={setDisableInput}
           />
         } else if (formStructure[nameLabel].canBeModified) {
-          return <GenericFormGroup 
-            nameLabel={nameLabel} 
+          return <GenericFormGroup
+            nameLabel={nameLabel}
             disableInput={disableInput ? disableInput.includes(index) : false}
             inputType={formStructure[nameLabel].inputType}
           />
@@ -62,7 +72,7 @@ const renderModalContent = (typeOfModal: LargeModalType, entityForm: EntityFormT
   }
 };
 
-const LargeModal = ({ titleModal, btnArray, typeOfModal, entityForm, onClose }: LargeModalProps) => {
+const LargeModal = ({ titleModal, btnArray, typeOfModal, entityForm, onClose, children }: LargeModalProps) => {
   const { disableInput, setDisableInput } = useDisableInput();
 
   return (
@@ -70,6 +80,8 @@ const LargeModal = ({ titleModal, btnArray, typeOfModal, entityForm, onClose }: 
       <div className="large-modal white">
         <h1 className="heading-form">{titleModal}</h1>
         <div className="form-group">{renderModalContent(typeOfModal, entityForm, disableInput, setDisableInput)}</div>
+        {}
+        {children}
         <div className="button-wrapper">
           {btnArray && btnArray.map((btn => btn))}
           <button

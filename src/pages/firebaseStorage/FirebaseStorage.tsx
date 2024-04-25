0d2fde7ee2bf3c7a182daba9235d76ea93b8fa5c
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-04-24 22:06:45 -0700

    Refactor: A better way to implement uploading files by logo/contract/profile

--- src/pages/firebaseStorage/FirebaseStorage.tsx
+++ src/pages/firebaseStorage/FirebaseStorage.tsx
@@ -3,37 +3,86 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const clientLogoPath = "clients/logo/";
-// aca por ejemplo habria un contractPath, por ejemplo
+const clientContractPath = "client/contract";
+const peopleProfilePath = "people/profile/";
 
 const FirebaseStorage = () => {
- const [file, setFile] = useState<File>();
- const [path, setPath] = useState<string>();
+ const [logoFile, setLogoFile] = useState<File>();
+ const [logoPath, setLogoPath] = useState<string>();
+ const [contractFile, setContractFile] = useState<File>();
+ const [contractPath, setContractPath] = useState<string>();
+ const [profileFile, setProfileFile] = useState<File>();
+ const [profilePath, setProfilePath] = useState<string>();
 
-const handleSubmit = async (e: React.FormEvent) => {
-    if (file && path) {
+const handleSubmitClient = async (e: React.FormEvent) => {
+    if (logoFile && logoPath && contractFile && contractPath) {
       e.preventDefault();
-      const result = await uploadFile(file, path);
-      console.log(result);
+      const urlLogo = await uploadFile(logoFile, logoPath);
+      console.log(urlLogo);
+      const urlContract = await uploadFile(contractFile, contractPath);
+      console.log(urlContract);
     }
  };
 
+ const handleSubmitPeople = async (e: React.FormEvent) => {
+  if (profileFile && profilePath) {
+    e.preventDefault();
+    const urlProfile = await uploadFile(profileFile, profilePath);
+    console.log(urlProfile);
+  }
+};
+
+
  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Test</h1>
-      <input
-        type="file"
-        name="ClientLogo"
-        id="ClientLogo"
-        onChange={(e) => {
-          if (e.target.files && e.target.files.length > 0) {
-            const file = e.target.files[0];
-            setFile(file);
-            setPath(clientLogoPath + uuidv4());
-          }
-        }}
-      />
-      <button>Subir</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmitClient}>
+        <h1>Formato para subir en Clientes</h1>
+        <label htmlFor="clientLogo">Logo</label>
+        <input
+          type="file"
+          name="ClientLogo"
+          id="ClientLogo"
+          accept="image/png, image/jpeg"
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0) {
+              setLogoFile(e.target.files[0]);
+              setLogoPath(clientLogoPath + uuidv4());
+            }
+          }}
+        />
+        <label htmlFor="clientContract">Contract</label>
+        <input
+          type="file"
+          name="ClientContract"
+          id="ClientContract"
+          accept=".pdf"
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0) {
+              setContractFile(e.target.files[0]);
+              setContractPath(clientContractPath + uuidv4());
+            }
+          }}
+        />
+        <button>Subir a la DB de Clientes</button>
+      </form>
+      <form onSubmit={handleSubmitPeople}>
+        <h1>Formato para subir en Empleados</h1>
+        <label htmlFor="EmployeeProfile">Profile Pic</label>
+        <input
+          type="file"
+          name="EmployeeProfile"
+          id="EmployeeProfile"
+          accept="image/png, image/jpeg"
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0) {
+              setProfileFile(e.target.files[0]);
+              setProfilePath(peopleProfilePath + uuidv4());
+            }
+          }}
+        />
+        <button>Subir a la DB de Personas</button>
+      </form>
+    </>    
  );
 };
 

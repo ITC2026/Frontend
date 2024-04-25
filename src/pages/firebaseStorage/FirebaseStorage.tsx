08a03dbd64a043462689c2330c784c2e13e8e876
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2024-04-24 19:37:35 -0700

    Feature: Upload Files in Firebase

--- src/pages/firebaseStorage/FirebaseStorage.tsx
+++ src/pages/firebaseStorage/FirebaseStorage.tsx
@@ -1,10 +1,39 @@
-const FirebaseStorage = () => {
-  return (
-    <>
-      <h1> TEST </h1>
-      <input type="file" name="" id=""  onChange={e => console.log(e.target.files)} />
-    </>
-  )
-}
+import { uploadFile } from "../../firebase/initialize";
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
-export default FirebaseStorage
+const clientLogoPath = "clients/logo/";
+
+const FirebaseStorage = () => {
+ const [file, setFile] = useState<File>();
+ const [path, setPath] = useState<string>();
+
+const handleSubmit = async (e: React.FormEvent) => {
+    if (file && path) {
+      e.preventDefault();
+      const result = await uploadFile(file, path);
+      console.log(result);
+    }
+ };
+
+ return (
+    <form onSubmit={handleSubmit}>
+      <h1>Test</h1>
+      <input
+        type="file"
+        name="ClientLogo"
+        id="ClientLogo"
+        onChange={(e) => {
+          if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            setFile(file);
+            setPath(clientLogoPath + uuidv4());
+          }
+        }}
+      />
+      <button>Subir</button>
+    </form>
+ );
+};
+
+export default FirebaseStorage;

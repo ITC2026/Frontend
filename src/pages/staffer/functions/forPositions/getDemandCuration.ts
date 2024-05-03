b47002de0d973f467986be0e07c3753071c58268
Frank0125 <a01252974@tec.mx>
2024-05-03 12:34:35 -0700

    Fixes: Demand Curation solved and fix button routing for postulation candidates

--- src/pages/staffer/functions/forPositions/getDemandCuration.ts
+++ src/pages/staffer/functions/forPositions/getDemandCuration.ts
@@ -1,16 +1,23 @@
 import { getClientById } from "../../../../api/ClientAPI";
 import { getProjectById } from "../../../../api/ProjectAPI";
+import { getPositionById } from "../../../../api/PositionAPI";
 
-const getDemandCuration = async (projectID : number, position: Position) => {
+const getDemandCuration = async (projectID: number, positionID: number) => {
     try {
+        const position = await getPositionById(positionID) as Position;
         const project = await getProjectById(projectID) as Project;
         const client = await getClientById(project.client_id as number) as Client;
-        console.log("clien" + client);
-        if (client?.high_growth === true && position.is_exclusive === true) {
+        const highGrowth = client?.high_growth as boolean;
+        if (!position ||!position.is_exclusive) {
+            console.error("Position not found or does not have an is_exclusive property");
+            return "No Demand Curation";
+        }
+        const isExclusive = position.is_exclusive as boolean;
+        if (highGrowth === true && isExclusive === true) {
             return "Strategic";
-        } else if (client?.high_growth === false && position.is_exclusive === true) {
+        } else if (highGrowth === false && isExclusive === true) {
             return "Committed";
-        } else  {
+        } else {
             return "Open";
         }
         return "No Demand Curation";

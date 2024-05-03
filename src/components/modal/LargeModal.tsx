5061cafd921e5feb10150432ccf323b83c479ebf
DanFc <danielclausen12@gmail.com>
2024-05-03 02:25:29 -0700

    errors solved

--- src/components/modal/LargeModal.tsx
+++ src/components/modal/LargeModal.tsx
@@ -6,8 +6,6 @@ import "./style/LargeModal.css";
 interface Props {
   titleModal: string;
   formContent: React.ReactElement;
-  subtitleModal?: string;
-
   header?: React.ReactElement;
 }
 
@@ -15,7 +13,6 @@ const LargeModal = ({ titleModal, formContent, header }: Props) => {
   return (
     <div className="overlay background-gray">
       <div className="large-modal white">
-        <div className="modal-header">{header}</div>
         <h1>{titleModal}</h1>
 
         {formContent}
@@ -25,30 +22,3 @@ const LargeModal = ({ titleModal, formContent, header }: Props) => {
 };
 
 export default LargeModal;
-
-// /* eslint-disable no-case-declarations */
-// import React from "react";
-// import "../../index.css";
-// import "./style/LargeModal.css";
-
-// interface Props {
-//   titleModal: string;
-//   subtitleModal?: string;
-//   formContent: React.ReactElement;
-//   header?: React.ReactElement;
-// }
-
-// const LargeModal = ({ titleModal, formContent, header }: Props) => {
-//   return (
-//     <div className="overlay background-gray">
-//       <div className="large-modal white">
-//         <div className="modal-header">{header}</div>
-//         <h1>{titleModal}</h1>
-
-//         {formContent}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LargeModal;
